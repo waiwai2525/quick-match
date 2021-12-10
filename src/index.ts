cb3d7d53958bc1622ff19844fbff08ac3143cb34
waiwai2525 <waiwai.cps@gmail.com>
2021-12-11 01:17:10 +0900

    add: discord.js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+import { Client, Intents } from "discord.js";
+
 require("dotenv").config();
 
-console.log(process.env.TOKEN);
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+const TOKEN = process.env.TOKEN;
+
+client.on("message", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+  if (message.content === "hello") {
+    message.channel.send("hello, world!");
+  }
+});
+
+client.login(TOKEN);
