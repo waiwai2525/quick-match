bbb988e6042f722afec239da9cfc66a23a8e4d4c
waiwai2525 <waiwai.cps@gmail.com>
2021-12-11 16:33:57 +0900

    feat: add ts-node-dev

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,36 @@
 "use strict";
-function print(text) {
-    console.log(text);
-}
-const message = "Hello, World!";
-print(message);
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+require("dotenv").config();
+const client = new discord_js_1.Client({
+    intents: [discord_js_1.Intents.FLAGS.GUILDS, discord_js_1.Intents.FLAGS.GUILD_MESSAGES],
+});
+const TOKEN = process.env.TOKEN;
+client.on("ready", async () => {
+    console.log("Ready");
+    client.user?.setActivity("/match", {
+        type: "PLAYING",
+    });
+    await client.application?.commands.set([
+        {
+            name: "match",
+            description: "新しいセッションを開始します。",
+        },
+    ]);
+});
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) {
+        return;
+    }
+    if (interaction.commandName === "match") {
+        const row = new discord_js_1.MessageActionRow().addComponents(new discord_js_1.MessageButton()
+            .setCustomId("ready")
+            .setStyle("PRIMARY")
+            .setLabel("READY"));
+        await interaction.channel?.send({
+            content: "準備完了なら押してください",
+            components: [row],
+        });
+    }
+});
+client.login(TOKEN);
